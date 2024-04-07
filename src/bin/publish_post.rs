9555e60f186ea1491dfc4ae6c79f5b9a29b7db4c
nqdinh-1203 <quangdinhnt1203@gmail.com>
2024-04-07 22:22:48 +0700

    first commit

---
+++ src/bin/publish_post.rs
@@ -0,0 +1,22 @@
+use std::env::args;
+
+use diesel::prelude::*;
+use diesel_demo::{models::*, *};
+
+fn main() {
+    use self::schema::posts::dsl::{posts, published};
+
+    let id = args()
+        .nth(1)
+        .expect("publish_post requires a post id")
+        .parse::<i32>()
+        .expect("Invalid Id");
+    let connection = &mut establish_connection();
+
+    let post = diesel::update(posts.find(id))
+        .set(published.eq(true))
+        .returning(Post::as_returning())
+        .get_result(connection)
+        .unwrap();
+    println!("Published post {}", post.title);
+}
---
+++ src/bin/show_posts.rs
@@ -0,0 +1,19 @@
+use diesel::prelude::*;
+use diesel_demo::{establish_connection, models::Post, schema::posts::dsl::*};
+
+fn main() {
+    let connection = &mut establish_connection();
+    let results = posts
+        .filter(published.eq(true))
+        .limit(5)
+        .select(Post::as_select())
+        .load(connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("-----------\n");
+        println!("{}", post.body);
+    }
+}
